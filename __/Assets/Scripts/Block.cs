6d760d339c48661548d3890bf8a734ed14756ca4
ARAKI <moon707.piano@icloud.com>
2021-07-12 15:09:33 +0900

    20210712

---
+++ __/Assets/Scripts/Block.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Block"))
+        Destroy(col.gameObject);
+    }
+}
---
+++ __/Assets/Scripts/GlobalStateManager.cs
@@ -0,0 +1,82 @@
+﻿/*
+ * Copyright (c) 2017 Razeware LLC
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
+ * distribute, sublicense, create a derivative work, and/or sell copies of the 
+ * Software in any work that is designed, intended, or marketed for pedagogical or 
+ * instructional purposes related to programming, coding, application development, 
+ * or information technology.  Permission for such use, copying, modification,
+ * merger, publication, distribution, sublicensing, creation of derivative works, 
+ * or sale is expressly withheld.
+ *    
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class GlobalStateManager : MonoBehaviour
+{
+    private int deadPlayers = 0;    // 死亡したプレイヤーの数
+    private int deadPlayerNumber = -1;  // 死亡したプレイヤーの番号
+
+    public void PlayerDied (int playerNumber)
+    {
+        // 死亡したプレイヤーの数を増やす
+        deadPlayers++;
+
+        // 3人のプレイヤーが死亡したら
+        if (deadPlayers == 3)
+        {
+            // 死亡したプレイヤーの番号を保持し
+            deadPlayerNumber = playerNumber;
+
+            // 0.3f秒後にCheckPlayersDeath関数を呼び出す
+            Invoke("CheckPlayersDeath", 0.3f);
+        }
+    }
+
+    void CheckPlayersDeath()
+    {
+        // 死亡したプレイヤーが1人だけの場合
+        if (deadPlayers == 3)
+        {
+            // プレイヤー1が死亡した場合
+            if (deadPlayerNumber == 3)
+            {
+                // プレイヤー2が勝利した
+                Debug.Log("プレイヤー2の勝利!!");
+            }
+            // プレイヤー2が死亡した場合
+            else
+            {
+                // プレイヤー1が勝利した
+                Debug.Log("プレイヤー1の勝利!!");
+                Debug.Log("プレイヤー3の勝利!!");
+                Debug.Log("プレイヤー4の勝利!!");
+            }
+        }
+        // すべてのプレイヤーが死亡した場合
+        else
+        {
+            // 引き分け
+            Debug.Log("引き分け");
+        }
+    }
+}
